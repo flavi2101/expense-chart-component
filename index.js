218d6ee823e367190b3bd0e309a4254b1c078f8e
Flavio Henrique Ferreira <flavi2101@gmail.com>
2022-05-31 17:37:57 -0300

    style - Set value balance and total month

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const weekDay = document.querySelectorAll("[data-week]");
+const balance = document.querySelector("[data-balance]");
+const balanceMonth = document.querySelector("[data-balance-month]");
 const sizeOfSreen = parseInt(
   window.getComputedStyle(weekDay[0])["block-size"],
   10
@@ -14,12 +16,16 @@ function convertToJson(response) {
 }
 
 function dataInformation(data) {
+  let balanceSum = 0;
   for (let i = 0; i < data.length; i++) {
     let value = `${parseInt((data[i].amount * 650) / sizeOfSreen)}px`;
     weekDay[i].style["block-size"] = value;
-    weekDay[i].setAttribute('data-value', `R$ ${data[i].amount}`)
-
+    weekDay[i].setAttribute("data-value", `R$ ${data[i].amount}`);
+    balanceSum += data[i].amount;
   }
+
+  balance.textContent = `$${balanceSum}`;
+  balanceMonth.textContent = `$${balanceSum}`;
 }
 
 reqData();

a8c8e345211c635378c0cd3c9442ad6611f41fd3
Flavio Henrique Ferreira <flavi2101@gmail.com>
2022-05-31 16:10:34 -0300

    setting render data js file

---
+++ index.js
@@ -0,0 +1,23 @@
+const weekDay = document.querySelectorAll("[data-week]");
+const sizeOfSreen = parseInt(
+  window.getComputedStyle(weekDay[0])["block-size"],
+  10
+);
+
+function reqData() {
+  fetch("data.json").then((res) =>
+    convertToJson(res).then((data) => dataInformation(data))
+  );
+}
+function convertToJson(response) {
+  return response.json();
+}
+
+function dataInformation(data) {
+  for (let i = 0; i < data.length; i++) {
+    let value = `${parseInt((data[i].amount * 850) / sizeOfSreen)}px`;
+    weekDay[i].style["block-size"] = value;
+  }
+}
+
+reqData();
